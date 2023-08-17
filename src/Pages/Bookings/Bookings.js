ec801a96b967d6ccf3e719d00f3dec510ea80414
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-08-17 23:11:05 +0600

    Update DataBase from Server

--- src/Pages/Bookings/Bookings.js
+++ src/Pages/Bookings/Bookings.js
@@ -14,7 +14,54 @@ const Bookings = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, [])
+    }, [url]);
+    const handleDelete =id =>{
+        
+        const proceed = window.confirm("Are you  sure want to delete")
+        if(proceed){
+             fetch(`http://localhost:5000/bookings/${id}`, {
+                method:"DELETE",
+                
+             })
+             .then(res => res.json())
+             .then(data =>{
+                console.log(data)
+                if(data.deletedCount > 0){
+                    alert('deleted Successful')
+                    const remaining  = bookings.filter(bookings=>bookings._id !== id);
+                    setBookings(remaining);
+                }
+             })
+            
+        }
+    }
+
+    const handleBookingConfirm = id =>{
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method:"PATCH",
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({status: 'confirm'})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.modifiedCount > 0){
+                // update state
+
+                const remaining = bookings.filter(booking => booking._id !== id);
+                const updated = bookings.find(booking => booking._id === id);
+                updated.status ="confirm";
+                const newBooking = [updated, ...remaining];
+                setBookings(newBooking);
+            }
+        })
+
+    }
+
+     
+
 
     return (
         <div>
@@ -31,17 +78,21 @@ const Bookings = () => {
                                     <input type="checkbox" className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            <th>Image</th>
+                            <th>Service</th>
+                            <th>Email</th>
+                            <th>Price</th>
+                            <th>Date</th>
+                            <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             bookings.map(booking => <BookingRow
                             key={booking._id}
-                            booking={booking}></BookingRow>)
+                            booking={booking}
+                            handleDelete={handleDelete}
+                            handleBookingConfirm={handleBookingConfirm}></BookingRow>)
                         }
                        
                             
